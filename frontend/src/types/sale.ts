5e67a3caa43ab63bf22405270f6d2d918d29c7e7
Luciano Umbelino <luciano.umbelino@gmail.com>
2021-09-12 19:10:01 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,25 @@
-import { type } from "os";
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: 41,
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller:Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number:number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
 
 export type SaleSum =  {
     sellerName: string;
